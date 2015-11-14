9fe42dffb720affa94821a776dc3c5de399b814e
Mitchell Monahan <soccermitchy@gmail.com>
2015-11-13 22:54:52 -0500

    Create a Blade config set for everything for a base Debian install, along with quick install instructions.

---
+++ Bladerunner.lua
@@ -0,0 +1,51 @@
+function target.config()
+	local name = "Generating live build configuration..."
+	local config_args = {
+		"--apt apt",
+		"--bootloader grub",
+		"--debian-installer-gui false",
+		"--distribution jessie",
+		"--iso-application DebVirt",
+		"--iso-preparer 'Mitchell Monahan'",
+		"--iso-publisher 'Mitchell Monahan'",
+		"--mode debian"
+	}
+	print(name)
+	local exitCode,stdout,stderr=blade.exec("lb config "..table.concat(config_args," "))
+	blade.printStatus(name,exitCode)
+end
+blade.help(target.config,"Generate configuration for live-build")
+
+function target.bootstrap(use)
+	local name = "Bootstrapping chroot..."
+	print(name)
+	local exitCode,stdout,stderr=blade.exec("lb bootstrap")
+	blade.printStatus(name,exitCode)
+	if exitCode==1 and stderr:find('need root privileges') then
+		print('This is a known issue with live-build, the tool used here to generate the distro. Fakeroot doesn\'t work, sadly. Tell me if you have any success in it and I\'ll add it to this script.')
+	end
+end
+blade.help(target.bootstrap,"Bootstrap the chroot with a debian install.")
+
+function target.chroot()
+	local name = "Customizing chroot..."
+	print(name)
+	local exitCode,stdout,stderr=blade.exec("lb chroot")
+	blade.printStatus(name,exitCode)
+	if exitCode==1 and stderr:find('need root privileges') then
+		print('This is a known issue with live-build, the tool used here to generate the distro. Fakeroot doesn\'t work, sadly. Tell me if you have any success in it and I\'ll add it to this script.')
+	end
+end
+blade.help(target.chroot,"Customize the chroot with the packages used in the distro.")
+
+function target.binary()
+	local name = "Generating binary..."
+	print(name)
+	local exitCode,stdout,stderr=blade.exec("lb binary")
+	blade.printStatus(name,exitCode)
+        if exitCode==1 and stderr:find('need root privileges') then
+		print('This is a known issue with live-build, the tool used here to generate the distro. Fakeroot doesn\'t work, sadly. Tell me if you have any success in it and I\'ll add it to this script.')
+	end
+
+end
+blade.help(target.binary,"Generate the iso file for the distro")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DebVirt
 Debian-based, lightweight OS for virtualization
+
+# Building
+1. Install [blade](https://otm.github.io/blade/) and live build (`sudo apt-get install live-build`)
+2. Generate the config for live-build: `blade config`
+3. Bootstrap the chroot: `blade bootstrap`
+4. Customise the chroot using the config generated earlier: `blade chroot`
+5. Build the binary (aka the iso): `blade binary`
